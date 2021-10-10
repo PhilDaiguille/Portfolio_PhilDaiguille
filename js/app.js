7d3407a266c92513ee46f36465ac6a405b5a86ad
PhilDaiguille <philippe.delente@gmail.com>
2021-10-10 15:59:01 +0200

    refresh JS

--- js/app.js
+++ js/app.js
@@ -2,7 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("loaded");
     let btn = document.querySelector(".material-icons");
     let el = document.querySelector("nav");
+    let date = document.querySelector("footer address ul li span");
     btn.addEventListener("click", () => {
         el.classList.toggle("open-menu");
     });
+
+    dates = new Date(), date.innerHTML = `${dates.getFullYear()}`;
 });
