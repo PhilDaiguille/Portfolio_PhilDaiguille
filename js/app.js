40b3551be06a8daf2c372af98f7febc2a531c6cc
PhilDaiguille <philippe.delente@gmail.com>
2022-04-29 15:01:49 +0200

    Update 6.5

--- js/app.js
+++ js/app.js
@@ -1,16 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("loaded");
-    let btn = document.querySelector(".burger");
-    let el = document.querySelector("header div");
-    let date = document.querySelector("footer p");
+	console.log("loaded");
+	let btn = document.querySelector(".burger");
+	let el = document.querySelector("header div");
+	let date = document.querySelector("footer p");
+	let down = document.querySelector("nav ul");
 
-    btn.addEventListener("click", () => {
-        el.classList.toggle("open-menu");
-        btn.innerHTML === "☰" ? btn.innerHTML = "✕" : btn.innerHTML = "☰";
-        
-    });
-    
-    let dates = new Date();
-    date.innerHTML = `Copyright © ${dates.getFullYear()} - Philippe DELENTE | Tous droits réservés`;
+	btn.addEventListener("click", () => {
+		el.classList.toggle("open-menu");
+		btn.innerHTML === "☰" ? (btn.innerHTML = "✕") : (btn.innerHTML = "☰");
+	});
 
+	down.addEventListener("click", () => {
+		el.classList.toggle("open-menu");
+		btn.innerHTML === "☰" ? (btn.innerHTML = "✕") : (btn.innerHTML = "☰");
+	});
+
+	let dates = new Date();
+	date.innerHTML = `Copyright © ${dates.getFullYear()} - Philippe DELENTE | Tous droits réservés`;
+
+	const sections = document.querySelectorAll("main section");
+	let options = {
+		rootMargin: "-10% 0px",
+		threshold: 0.4,
+	};
+	function handleIntersect(entries) {
+		console.log(entries);
+
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.style.opacity = 1;
+			}
+		});
+	}
+	const observer = new IntersectionObserver(handleIntersect, options);
+
+	sections.forEach((section) => {
+		observer.observe(section);
+	});
 });
