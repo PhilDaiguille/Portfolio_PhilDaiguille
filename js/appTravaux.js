dc1bbed1956a1a659ccef9a6edb37e32750fcc9f
philippe <philippe.delente@gmail.com>
2023-03-02 16:26:24 +0100

    Portofolio Beta V.8

--- js/appTravaux.js
+++ js/appTravaux.js
@@ -5,33 +5,21 @@ document.addEventListener("DOMContentLoaded", () => {
 	let dates = new Date();
 	date.innerHTML = `Copyright © ${dates.getFullYear()} - Philippe DELENTE | Tous droits réservés`;
 
-	// slider
-	let slider = document.getElementsByClassName("slider");
-	let page = document.querySelector(".page");
-	let etape = 0;
-	let nbimg = slider.length;
-	page.innerHTML = `Page ${etape + 1}/${nbimg}`;
-	let precedent = document.querySelector(".precedent");
-	let suivant = document.querySelector(".suivant");
+	const swiper = new Swiper(".swiper", {
+		slidesPerView: 2,
+		spaceBetween: 50,
+		loop: true,
+		grabCursor: true,
+		centeredSlides: true,
+		slideActiveClass: "active",
+		navigation: {
+		  nextEl: ".next",
+		  prevEl: ".prev"
+		},
+		pagination: {
+		  el: ".pagination",
+		  clickable: true
+		},
+	  });
 
-	let enleverActiveImages = () => {
-		for (let i = 0; i < nbimg; i++) {
-			slider[i].classList.remove("active");
-		}
-		page.innerHTML = `Page ${etape + 1}/${nbimg}`;
-	};
-
-	suivant.addEventListener("click", () => {
-		etape++;
-		etape = etape >= nbimg ? 0 : etape;
-		enleverActiveImages();
-		slider[etape].classList.add("active");
-	});
-
-	precedent.addEventListener("click", () => {
-		etape--;
-		etape = etape < 0 ? nbimg - 1 : etape;
-		enleverActiveImages();
-		slider[etape].classList.add("active");
-	});
 });
